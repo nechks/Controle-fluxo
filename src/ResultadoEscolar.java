383c4af67ab02cc258512d3e12136e75b1c54dfb
YuriM <yurimartinelli123@gmail.com>
2023-11-07 19:43:09 -0300

    finalizado

--- src/ResultadoEscolar.java
+++ src/ResultadoEscolar.java
@@ -1,12 +1,18 @@
 public class ResultadoEscolar {
     public static void main(String[] args) {
-        int nota = 6;
+        int nota = 3;
 
         if (nota >= 7)
            System.out.println("Aprovado!");
+
+           else if (nota >= 5 && nota < 7) {
+            System.out.println("Prova Recuperação");
+            
+           }
         
         else
          System.out.println("Reprovado!");
+
     
     }
 }
