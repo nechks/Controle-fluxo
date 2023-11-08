b44a7f7fc5100391e67ee922f9d818917d00ebdb
YuriM <yurimartinelli123@gmail.com>
2023-11-07 21:03:35 -0300

    condiçao ténarias

---
+++ src/ResultadoEscolaa.java
@@ -0,0 +1,9 @@
+public class ResultadoEscolaa {
+    public static void main(String[] args) {
+        int nota = 5;
+        String resultado = nota >=7 ? "Aprovado" : nota >= 5 && nota <7 ? "Reuperação" : "Reprovado";
+        System.out.println(resultado);
+    }
+}
+
+// ? (se) : (senão) && (e)
--- src/ResultadoEscolar.java
+++ src/ResultadoEscolar.java
@@ -1,6 +1,6 @@
 public class ResultadoEscolar {
     public static void main(String[] args) {
-        int nota = 3;
+        int nota = 7;
 
         if (nota >= 7)
            System.out.println("Aprovado!");
